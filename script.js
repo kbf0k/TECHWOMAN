836e2dcd13c652227e8c89706678e58aff7721ee
Kaique <kaique1245br@gmail.com>
2025-03-13 15:22:06 -0300

    Commit FInal

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
-        document.querySelector('header').style.background = 'rgb(255, 96, 122)';
+        document.querySelector('header').style.background = 'rgb(100, 3, 19)';
         document.querySelector('header').style.boxShadow = '0 0 25px black';
-        document.querySelector('header').style.border = '1px solid rgb(255, 96, 122) ';
+        document.querySelector('header').style.border = '1px solid rgb(100, 3, 19) ';
     }
     else {
         document.querySelector('header').style.background = 'transparent';
         document.querySelector('header').style.boxShadow = ''
         document.querySelector('header').style.border = '';
     }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".info-section");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            } else {
+                entry.target.classList.remove("visible");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    sections.forEach((section) => observer.observe(section));
 });
